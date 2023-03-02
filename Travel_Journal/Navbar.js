de99105dc20a469ff7d640fa25e15bc9f205927c
Rushikesh Patil <98112083+rushikesh3405@users.noreply.github.com>
2023-03-02 18:00:00 +0530

    Add files via upload

---
+++ Travel_Journal/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Navbar(){
+    return(
+        <>
+        <div className="nav">
+            <img src="../images/logo.png" alt="logo" className="nav--logo" ></img>
+            <p className="nav--text">MY TRAVEL JOURNAL</p>
+        </div>
+        </>
+    )
+}
---
+++ Travel_Journal/data.js
@@ -0,0 +1,74 @@
+const data = [
+    {   id : 1,
+        title:"Kolhapur Torisum",
+        location:"Kolhapur",
+        mapslink:"http://www.timothytye.com/asia/india/kolhapur.htm",
+        startdate: "2022-07-17",
+        enddate:  "2022-07-19",
+        img:"../images/kolhapur.jpg",
+        desc:"Kolhapur is a city on the banks of the Panchaganga River, in the west Indian state of Maharashtra. Its known for its temples, like the ancient Mahalakshmi Temple, a Hindu pilgrimage site. The Bhavani Mandap is an imposing old palace with a small museum displaying stuffed wild animals. Close by, Rankala Lake is surrounded by gardens. The hilltop Jyotiba Temple complex is on the city's outskirts."
+    },
+
+    {   id : 2,
+        title:"Lonavala Darshan",
+        location:"Lonavala",
+        mapslink:"https://www.google.com/maps/place/Lonavala/@18.7495336,73.4073874,17z/data=!3m1!4b1!4m5!3m4!1s0x0:0xb297152aff62735a!8m2!3d18.7495336!4d73.4073874",
+        startdate: "2022-07-17",
+        enddate:  "2022-07-17",     
+        img:"../images/lonavala.jpg",
+        desc:"Lonavala is a hill station surrounded by green valleys in western India near Mumbai. The Karla Caves and the Bhaja Caves are ancient Buddhist shrines carved out of the rock. They feature massive pillars and intricate relief sculptures. South of the Bhaja Caves sits the imposing Lohagad Fort, with its 4 gates."
+    },
+
+    {   id : 3,
+        title:"Nagpur trip",
+        location:"Nagpur",
+        mapslink:"http://www.timothytye.com/asia/india/nagpur.htm",
+        startdate: "2022-07-17",
+        enddate:  "2022-07-17",     
+        img:"../images/Nagpur.jfif",
+        desc:"Nagpur is a large city in the central Indian state of Maharashtra. The 19th-century Nagpur Central Museum displays items found locally, including fossils, sarcophagi and Mughal weaponry. The Raman Science Centre has hands-on exhibits and a planetarium. Sitabuldi Fort, in the Sitabuldi Hills, was the site of an 1817 battle."
+    },
+
+    {   id : 4,
+        title:"Raigad Torisum",
+        location:"Raigad district",
+        mapslink:"https://goo.gl/maps/SskASgdfwGSbGqGr6",
+        startdate: "2022-07-17",
+        enddate:  "2022-07-17",     
+        img:"../images/raigad.jfif",
+        desc:"Raigad district, previously Colaba district, is a district in the Konkan division of Maharashtra, India. The district was renamed to Raigad after the fort that was the first capital of the former Maratha Empire, which in turn was renamed from its earlier name - Rairi."
+    },
+
+    {   id : 5,
+        title:"Pune darshan",
+        location:"Pune",
+        mapslink:"https://goo.gl/maps/oraesVXMtNaNgjsm9",
+        startdate: "2022-07-17",
+        enddate:  "2022-07-17",     
+        img:"../images/Pune.jfif",
+        desc:"Pune is a sprawling city in the western Indian state of Maharashtra. It was once the base of the Peshwas (prime ministers) of the Maratha Empire, which lasted from 1674 to 1818. It's known for the grand Aga Khan Palace, built in 1892 and now a memorial to Mahatma Gandhi, whose ashes are preserved in the garden"
+    },
+
+    {   id : 6,
+        title:"Pandharapur Torisum",
+        location:"Pandharapur",
+        mapslink:"https://goo.gl/maps/X8zGfqGEWUq9fDfo9",
+        startdate: "2022-07-17",
+        enddate: "2022-07-17",     
+        img:"../images/pandharpur.jpg",
+        desc:"Pandharpur is a well known pilgrimage town, on the banks of Candrabhagā River, near Solapur city in Solapur District, Maharashtra, India."
+    },
+
+    {   id : 7,
+        title:"mahabaleshwar Torisum",
+        location:"Mahabaleshwar",
+        mapslink:"https://goo.gl/maps/19yfAwbRJDkgzDBEA",
+        startdate: "2022-07-17",
+        enddate:  "2022-07-17",     
+        img:"../images/mahabal.jfif",
+        desc:"Mahabaleshwar is a hill station in India's forested Western Ghats range, south of Mumbai. It features several elevated viewing points, such as Arthurs Seat."
+    }
+
+]
+
+export default data
