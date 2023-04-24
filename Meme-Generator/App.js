0096845763cec1dd581900d5b589f90f6f35a143
Rushikesh Patil <98112083+rushikesh3405@users.noreply.github.com>
2023-04-24 23:30:05 +0530

    Update App.js

--- Meme-Generator/App.js
+++ Meme-Generator/App.js
@@ -2,14 +2,30 @@ import React from 'react'
 import './style.css'
 import Header from "./components/Header.js"
 import Meme from "./components/Meme.js"
-import memesdata from './memedata'
-import Star from "./components/Star.js"
-import Box from "./components/Box.js"
-import boxes from './components/boxes.js'
-import Form from "./components/Form.js"
 
 
 export default function App(){
+    const[meme,setMeme] = React.useState({
+    topText : "",
+    bottomText : "",
+    randomImg : "http://api.imgflip.com/lbij.jpg"
+  })
+  const [allMemes,setAllMemes] = React.useState([]);  
+
+  React.useEffect(function(){
+    fetch(`http://api.imgflip.com/get_memes`)
+    .then(res => res.json())
+    .then(data => setAllMemes(data.data.memes))
+  },[])
+
+  function getMemeImage(){
+    const randomNumber = Math.floor(Math.random()*allMemes.length());
+    const url = allMemes[randomNumber].url
+    setMeme(prevmeme =>({
+      ...prevmeme,
+      randomImg : url
+    }))
+  }
   return(
     <div>
     <Header/>
